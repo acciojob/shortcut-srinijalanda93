b3108f789800a1accbd478a9aaf98272944ef349
srinija landa <144719297+srinijalanda93@users.noreply.github.com>
2023-12-19 22:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function shortcut(s1, s2) {
-  // your code here
+  if (!s1 || !s2) {
+    return '';
+  }
+
+  // Get the initial letter of each string and concatenate them
+  const result = s1.charAt(0).toUpperCase() + s2.charAt(0).toLowerCase();
+
+  return result;
 }
 
-// Do not change the code below.
+// Example usage
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
